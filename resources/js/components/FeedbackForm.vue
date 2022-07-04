da7fe87763fe9671118faa230017693226640040
ilg2005 <igoldfarb10@gmail.com>
2022-07-04 13:53:51 +0300

    разметка формы и стили

---
+++ resources/js/components/FeedbackForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+
+        <div class="container">
+            <h1 class="text-center mb-3">{{ msg }}</h1>
+            <form method="post" action="/">
+                <div class="form-group">
+                    <label>Имя</label>
+                    <input type="text" class="form-control" name="name" id="name">
+                    <div class="error">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>E-mail</label>
+                    <input type="email" class="form-control" name="email" id="email">
+                    <div class="error">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Телефон</label>
+                    <input type="text" class="form-control" name="phone" id="phone">
+                    <div class="error">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Тема</label>
+                    <input type="text" class="form-control" name="subject"
+                           id="subject">
+                    <div class="error">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Сообщение</label>
+                    <textarea class="form-control" name="message" id="message"
+                              rows="4"></textarea>
+                    <div class="error">
+                    </div>
+                </div>
+                <input type="submit" name="send" value="Отправить" class="btn btn-primary btn-block">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FeedbackForm',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./css/app.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
     <title>Напишите нам</title>
 </head>
 <body>
-
-<script src="./public/js/app.js"></script>
+<div id="app">
+    <feedback-form msg="Напишите нам:"/>
+</div>
+<script src="./js/app.js"></script>
 </body>
 </html>
+
